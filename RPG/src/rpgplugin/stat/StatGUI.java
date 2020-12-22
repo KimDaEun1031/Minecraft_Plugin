affa7c79610a7a0329017185cc74d786d03646a1
KimDaEun1031 <kimde1031@gmail.com>
2020-12-23 02:57:37 +0900

    12/23 number 3 in

---
+++ RPG/src/rpgplugin/stat/StatGUI.java
@@ -0,0 +1,17 @@
+package rpgplugin.stat;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+
+public class StatGUI {
+	public Stat s = new Stat();
+	
+	public void StatusGUI(Player player) {
+		long [] i = new long[12];
+		i = s.getStat(player.getUniqueId().toString());
+		
+		Inventory inv = Bukkit.createInventory(null, 45, "스텟");
+		//3번 볼때임
+	}
+}
